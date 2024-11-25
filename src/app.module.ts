a79d4478aab86da45dcae9df62f806ac0f0b33f4
minhhuy230301 <huyhoavang1231@gmail.com>
2024-11-25 19:03:06 +0700

    huy fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { QuestionModule } from './question/question.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    AdminModule,
+    UserModule,
+    AuthModule,
+    CommentModule,
+    QuestionModule,
+    AnswerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
