4ccee451483292179fc216dee996ec4ac928c621
phanngotuankiet <ngdacthinh0912@gmail.com>
2024-12-21 16:36:15 +0700

    tang thoi gian het han token va refresh token

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
